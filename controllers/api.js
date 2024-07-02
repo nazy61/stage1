8b5db9c097d2781935276c678a5e242d696c5ddb
Ogwo Chinaza <ogwo.chinaza@yahoo.com>
2024-07-02 20:49:00 +0100

    api done

--- controllers/api.js
+++ controllers/api.js
@@ -1,4 +1,6 @@
 const { SuperfaceClient } = require("@superfaceai/one-sdk");
+const sdk = new SuperfaceClient();
+const axios = require("axios");
 
 module.exports.user = async (req, res) => {
   const { visitor_name } = req.query;
@@ -7,36 +9,52 @@ module.exports.user = async (req, res) => {
     // Get the client's IP address
     let ipAddress = req.ip;
 
-    // Get location data from IP address
-    const sdk = new SuperfaceClient();
-    const profile = await sdk.getProfile("address/ip-geolocation@1.0.1");
-
-    // Use the profile
-    const result = await profile.getUseCase("IpGeolocation").perform(
-      {
-        ipAddress: ipAddress,
-      },
-      {
-        provider: "ipdata",
-        security: {
-          apikey: {
-            apikey: "9a511b6fc8334e1852cfbbd4ff3f1af3c42ed6abc75e96a1648b969a",
-          },
-        },
-      }
-    );
+    // Get Region
+    const region = await getRegion(ipAddress);
+    const weather = await getWeather(region);
 
     // Send a response with the visitor's name and location data
     res.send({
       client_ip: ipAddress,
-      location: result.unwrap(),
-      message: `Hello, ${visitor_name}!`,
+      location: region.addressRegion,
+      greeting: `Hello, ${visitor_name}!, the temperature is ${weather.current.temp_c} degrees Celcius in ${region.addressRegion}`,
     });
   } catch (error) {
-    console.error(error);
     res.status(400).json({
       status: false,
       message: "An error occurred while fetching location data",
+      error: error,
     });
   }
+};
+
+const getWeather = async (region) => {
+  // Get weather data from region open weather map using axios
+  const weather = await axios.get(
+    `https://api.weatherapi.com/v1/current.json?key=${process.env.OPENWEATHER_API_KEY}&q=${region.addressRegion}&aqi=no`
+  );
+
+  return weather.data;
+};
+
+const getRegion = async (ipAddress) => {
+  // Get location data from IP address
+  const profile = await sdk.getProfile("address/ip-geolocation@1.0.1");
+
+  // Use the profile
+  const result = await profile.getUseCase("IpGeolocation").perform(
+    {
+      ipAddress: ipAddress,
+    },
+    {
+      provider: "ipdata",
+      security: {
+        apikey: {
+          apikey: process.env.IPDATA_API_KEY,
+        },
+      },
+    }
+  );
+
+  return result.unwrap();
 };
